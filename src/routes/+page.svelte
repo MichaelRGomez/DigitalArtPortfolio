4c7257b63646db60a17ae1b354e396ba44b61fa0
Michael <2018118090@ub.edu.bz>
2024-03-14 11:27:10 -0600

    added stuff the fullarchive

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,6 +7,9 @@ px-4 py-2
 ">
     <h1 class="text-2xl font-mono font-bold underline">Home</h1>
     <hr class=""/>
+    <!-- TODO: figure out the art component and
+    remember that home will only render the most recent 10 art posts
+    -->
     <h2>Recent Art, Artists and Yoinks</h2>
     <div>
         <div>
--- src/routes/FullArchive/+page.svelte
+++ src/routes/FullArchive/+page.svelte
@@ -1,1 +1,23 @@
-<h1>Full Archive</h1>
+<div
+class="
+lg:flex
+inline-flex
+flex-col
+px-4 py-2
+">
+    <h1 class="text-2xl font-mono font-bold underline">Full Archive</h1>
+    <hr class=""/>
+    <!-- TODO: figure out the art component and
+    full archive will page-nate all the art posts 15 posts at a time
+    -->
+    <h2>Recent Art, Artists and Yoinks</h2>
+    <div>
+        <div>
+            <h3>Title of Post</h3>
+            <div>
+                Media type
+            </div>
+            <p>Any filler text I want to add</p>
+        </div>
+    </div>
+</div>
