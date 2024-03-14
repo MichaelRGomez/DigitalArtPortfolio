9aa8f0733ed502b8a8dd5556a61b48addf94231d
Michael <2018118090@ub.edu.bz>
2024-03-14 11:11:03 -0600

    preparing to figure the art component

---
+++ src/models/art.ts
@@ -0,0 +1,5 @@
+export interface Art{
+    title: string;
+    pathToFile: string; //TODO: figure this out in the future
+    caption: string;
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,20 @@
-<h1>Home</h1>
+<div
+class="
+lg:flex
+inline-flex
+flex-col
+px-4 py-2
+">
+    <h1 class="text-2xl font-mono font-bold underline">Home</h1>
+    <hr class=""/>
+    <h2>Recent Art, Artists and Yoinks</h2>
+    <div>
+        <div>
+            <h3>Title of Post</h3>
+            <div>
+                Media type
+            </div>
+            <p>Any filler text I want to add</p>
+        </div>
+    </div>
+</div>
